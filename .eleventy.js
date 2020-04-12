382053bd4cf3806ac2a7bb6dfe4167b02be213ba
Allan Ebdrup <allan@878.dk>
2020-04-12 12:09:15 +0200

    11ty (#4)

---
+++ .eleventy.js
@@ -0,0 +1,29 @@
+let Nunjucks = require('nunjucks');
+const CleanCSS = require('clean-css');
+const htmlmin = require('html-minifier');
+
+module.exports = function (eleventyConfig) {
+  // Copy `img/` to `_site/img`
+  eleventyConfig.addPassthroughCopy('img');
+  eleventyConfig.addPassthroughCopy('favicon*');
+  let nunjucksEnvironment = new Nunjucks.Environment(new Nunjucks.FileSystemLoader('_includes'));
+
+  eleventyConfig.setLibrary('njk', nunjucksEnvironment);
+
+  eleventyConfig.addFilter('cssmin', function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  // Minify HTML output
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (outputPath.indexOf('.html') > -1) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+    return content;
+  });
+};
