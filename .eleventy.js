8d439527c96dd52b37092f8a1b77de9aec934a90
Allan Ebdrup <allan@878.dk>
2020-04-12 11:02:10 +0200

    11ty

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 let Nunjucks = require('nunjucks');
 const CleanCSS = require('clean-css');
+const htmlmin = require('html-minifier');
 
 module.exports = function (eleventyConfig) {
   // Copy `img/` to `_site/img`
@@ -11,5 +12,18 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addFilter('cssmin', function (code) {
     return new CleanCSS({}).minify(code).styles;
+  });
+
+  // Minify HTML output
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (outputPath.indexOf('.html') > -1) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+    return content;
   });
 };
