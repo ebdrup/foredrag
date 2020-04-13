85c95aed9c120c31337070073d3b01a817cea5d6
Allan Ebdrup <allan@878.dk>
2020-04-13 09:34:43 +0200

    defer youtube

--- _js/deferYoutube.js
+++ _js/deferYoutube.js
@@ -1,7 +1,9 @@
-var vidDefer = document.getElementsByTagName('iframe');
-for (var i = 0; i < vidDefer.length; i++) {
-  if (vidDefer[i].getAttribute('data-src')) {
-    vidDefer[i].setAttribute('src', vidDefer[i].getAttribute('data-src'));
-    vidDefer[i].className = vidDefer[i].className.replace('hide', '');
+window.onload = function () {
+  var vidDefer = document.getElementsByTagName('iframe');
+  for (var i = 0; i < vidDefer.length; i++) {
+    if (vidDefer[i].getAttribute('data-src')) {
+      vidDefer[i].setAttribute('src', vidDefer[i].getAttribute('data-src'));
+      vidDefer[i].className = vidDefer[i].className.replace('hide', '');
+    }
   }
-}
+};
