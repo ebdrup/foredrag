780e9ec1d30a6db4325db7656c16860cadc55155
Allan Ebdrup <allan@878.dk>
2020-04-13 09:50:16 +0200

    try to fix webp

--- _js/defer.js
+++ _js/defer.js
@@ -1,4 +1,11 @@
-window.onload = function () {
+if (window.addEventListener) {
+  // W3C standard
+  window.addEventListener('load', defer, false);
+} else if (window.attachEvent) {
+  // Microsoft
+  window.attachEvent('onload', defer);
+}
+function defer() {
   setTimeout(function () {
     load(document.getElementsByTagName('iframe'));
     load(document.getElementsByTagName('script'));
@@ -12,4 +19,4 @@ window.onload = function () {
       }
     }
   }
-};
+}
--- _js/webp.js
+++ _js/webp.js
@@ -1,14 +1,24 @@
-var elem = document.createElement('canvas');
-var hasWebp =
-  !!(elem.getContext && elem.getContext('2d')) &&
-  elem.toDataURL('image/webp').indexOf('data:image/webp') === 0;
-if (!hasWebp) {
-  // no WebP
-  var items = document.getElementsByClassName('webp');
-  for (var i = 0; i < items.length; i++) {
-    items[i].style.backgroundImage = getComputedStyle(items[i]).backgroundImage.replace(
-      '.webp',
-      '',
-    );
+if (window.addEventListener) {
+  // W3C standard
+  window.addEventListener('load', webp, false);
+} else if (window.attachEvent) {
+  // Microsoft
+  window.attachEvent('onload', webp);
+}
+
+function webp() {
+  var elem = document.createElement('canvas');
+  var hasWebp =
+    !!(elem.getContext && elem.getContext('2d')) &&
+    elem.toDataURL('image/webp').indexOf('data:image/webp') === 0;
+  if (!hasWebp) {
+    // no WebP
+    var items = document.getElementsByClassName('webp');
+    for (var i = 0; i < items.length; i++) {
+      items[i].style.backgroundImage = getComputedStyle(items[i]).backgroundImage.replace(
+        '.webp',
+        '',
+      );
+    }
   }
 }
