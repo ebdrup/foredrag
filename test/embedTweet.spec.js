b99a98d2a8c390d1b42a724d9f12115d545b8b27
Allan Ebdrup <allan@878.dk>
2020-04-15 16:24:55 +0200

    simpleEmbedTweet

--- test/embedTweet.spec.js
+++ test/embedTweet.spec.js
@@ -2,7 +2,7 @@ const embedTweet = require('../_filters/embedTweet');
 
 const url = 'https://twitter.com/code_conf/status/651724049463312384';
 
-describe.only(`fetching tweet ${url} for embedding`, () => {
+describe(`fetching tweet ${url} for embedding`, () => {
   let res;
   before(async () => {
     res = await embedTweet(url, { forceReload: true });
