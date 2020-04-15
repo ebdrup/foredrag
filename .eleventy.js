348fbc509abd0355a46f3004b2a9b74e203c7345
Allan Ebdrup <allan@878.dk>
2020-04-15 20:24:23 +0200

    remove commented out console log

--- .eleventy.js
+++ .eleventy.js
@@ -19,7 +19,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addNunjucksAsyncFilterPromise = (name, fn) => {
     eleventyConfig.addNunjucksAsyncFilter(name, (...args) => {
       const cb = args.pop();
-      //console.log(this.ctx.page.inputPath);
       fn.apply(this, args)
         .then((...args) => cb.apply(null, [null, ...args]))
         .catch(cb);
